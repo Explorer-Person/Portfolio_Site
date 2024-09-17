399f46c08a30ce1017332ab9a5c576d16b98f31f
Explorer-Person <fatihe307@gmail.com>
2024-09-17 17:29:20 +0300

    changes

--- Portfolio-Backend/src/middlewares/cors/middleware.js
+++ Portfolio-Backend/src/middlewares/cors/middleware.js
@@ -1,39 +1,20 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
+const middleware = express();
 
-const app = express();
-
-// Middleware configuration based on environment
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (process.env.NODE_ENV === 'development') {
-      // Allow all origins in development mode
-      callback(null, true);
-    } else {
-      // Restrict origins in production mode
-      if (process.env.CLIENT.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  allowedHeaders: 'Content-Type,Authorization',
+  origin: [ 
+    `${process.env.CLIENT}`
+  ], // Only allow these origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  exposedHeaders: ["set-cookie"],
+  credentials: true, // Allow credentials like cookies
+  optionsSuccessStatus: 204 // Response status for successful OPTIONS requests
 };
 
-// Apply CORS middleware
-app.use(cors(corsOptions));
+middleware.use(cors(corsOptions));
 
-// Error handling for CORS
-app.use((err, req, res, next) => {
-  if (err) {
-    console.error('CORS Error:', err.message);
-    res.status(403).json({ message: err.message });
-  } else {
-    next();
-  }
-});
 
-module.exports = app;
+module.exports = middleware;
